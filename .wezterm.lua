528740d21a5998bd0e50c4c002323dba5c9d5680
sig49san <sibahara3@gmail.com>
2023-11-10 16:49:44 +0900

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- カラースキームの設定
+config.color_scheme = 'AdventureTime'
+-- 背景透過
+config.window_background_opacity = 0.85
+-- フォントの設定
+config.font = wezterm.font("RictyDiminishedDiscord Nerd Font", {weight="Medium", stretch="Normal", style="Normal"})
+-- フォントサイズの設定
+config.font_size = 16
+-- ショートカットキー設定
+local act = wezterm.action
+config.keys = {
+  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
+  {
+    key = 'f',
+    mods = 'SHIFT|META',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  -- Ctrl+Shift+tで新しいタブを作成
+  {
+    key = 't',
+    mods = 'SHIFT|CTRL',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  },
+  -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
+  {
+    key = '|',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+}
+
+config.leader = { key="a", mods="CTRL", timeout_milliseconds=1000 }
+
+return config
+
