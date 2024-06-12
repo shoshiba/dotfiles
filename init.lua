7ff2e48da9aecbde79dfbbc887be9a7202a4cbba
sig49san <sibahara3@gmail.com>
2024-06-13 01:58:11 +0900

    lazy_nvimをファイル分割。

--- init.lua
+++ init.lua
@@ -1,29 +1,4 @@
-
--- lazy.nvim用
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-  -- quick scope
-  'unblevable/quick-scope',
-
-  -- airline
-  'vim-airline/vim-airline',
-  'vim-airline/vim-airline-themes',
-
-  -- iceberg
-  'cocopon/iceberg.vim',
-})
-
+require("lazy_nvim")
 require("options")
 require("keymaps")
 --require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,12 @@
+
+return {
+  -- quick scope
+  'unblevable/quick-scope',
+
+  -- airline
+  'vim-airline/vim-airline',
+  'vim-airline/vim-airline-themes',
+
+  -- iceberg
+  'cocopon/iceberg.vim',
+}
