2e5274e66d66915ace035aed64c228d168a0e185
sig49san <sibahara3@gmail.com>
2024-06-13 01:51:28 +0900

    いったんklazyに

--- init.lua
+++ init.lua
@@ -1,7 +1,35 @@
+
+-- lazy.nvim用
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  -- quick scope
+  'unblevable/quick-scope',
+
+  -- airline
+  'vim-airline/vim-airline',
+  'vim-airline/vim-airline-themes',
+
+  -- iceberg
+  'cocopon/iceberg.vim',
+})
+
 require("options")
 require("keymaps")
-require("plugins")
--- require("colorscheme")
+--require("plugins")
+require("colorscheme")
+
+
 
 -- プラグイン管理
 --vim.cmd [[
@@ -46,9 +74,9 @@ require('obsidian-bridge').setup({
 })
 --
 ---- Airline設定
---vim.g.airline_theme = 'dark'               
---vim.g.airline_extensions_tabline_enabled = 1 
---vim.g.airline_powerline_fonts = 1            
+vim.g.airline_theme = 'dark'
+vim.g.airline_extensions_tabline_enabled = 1 
+vim.g.airline_powerline_fonts = 1 
 --
 
 -- IMEから抜ける設定
